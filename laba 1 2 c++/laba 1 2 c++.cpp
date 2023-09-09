964bc0612f756e72e89285d91791d8dd46d24049
nonplex <fuksvladislav@gmail.com>
2023-09-09 22:32:55 +0700

    Добавьте файлы проекта.

---
+++ laba 1 2 c++/laba 1 2 c++.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <clocale>
+
+using namespace std;
+
+/**
+ * @brief Л/р 1 Знакомство со средой разработки
+ * Исходный код на C++
+ * @return Всегда 0
+*/
+int main() {
+    setlocale(LC_ALL, "ru");
+
+    cout << "Hello World! Привет мир!" << endl;
+}
